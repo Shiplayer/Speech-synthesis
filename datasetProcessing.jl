ff64aed46422110f07f44a0ad15f29f2430b589a
Shiplayer <lllytep199515@yandex.ru>
2016-03-17 22:55:13 +0300

    Initial commit

---
+++ datasetProcessing.jl
@@ -0,0 +1,15 @@
+DatasetPath = "data/etc/txt.done.data"
+WavPath = "data/orig/"
+
+dataset = Dict{AbstractString, AbstractString}()
+count = 0
+for l in readlines(open(DatasetPath))
+	m = match(r"(?P<wav>a\w*\d*\S)\s[\"](?P<text>.*)[\"]", l)
+	setindex!(dataset, m[2], m[1]);
+end
+dataset = sort(collect(dataset))
+for (k,v) in dataset
+	println("$k.wav ($v)")
+end
+
+
