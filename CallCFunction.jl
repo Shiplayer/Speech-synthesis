d86bbbd3c11a4ce9e72f15b81f996dc8d056a4f6
Shiplayer <lllytep199515@yandex.ru>
2016-04-16 20:19:58 +0300

    remove c code

--- CallCFunction.jl
+++
@@ -1,4 +0,0 @@
-var = ccall((:test, "./cDir/test.so"), Ptr{UInt8}, ())
-print(unsigned(var), "\n")
-print(typeof(var),"\n")
-#print(typeof(var), " ", bytestring(var), "\n")
--- cDir/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-
-int* test(){
-	int* arr = {1;2;3;4};
-	return {1,2,3,4};
-}
