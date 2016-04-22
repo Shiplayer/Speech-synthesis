e03b2553a986d1b78b264165f44755b395746a49
Shiplayer <lllytep199515@yandex.ru>
2016-04-22 21:00:22 +0300

    c_v00002

--- cDir/test.c
+++ cDir/test.c
@@ -1,6 +1,27 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-int* test(){
-	int* arr = {1;2;3;4};
-	return {1,2,3,4};
+int* test(int num, int** array){
+	for(int i = 0; i < 10; i++){
+		printf("%d ", array[i][1]);
+	}
+	printf("\n%d\n", num);
+	int* arr = malloc(sizeof(int) * 10);
+	for(int i = 0; i < 10; i++){
+		arr[i] = 0;
+		if(i < num)
+			arr[i] = array[i][0] + array[i][1];
+		else
+			arr[i] = i;
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return arr;
+}
+
+int main(int argc, char** argv){
+	printf("%d\n", argc);
+	for(int i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return 0;
 }
