1c7b1446e900103f6b66941c40a3162e13bd3d8e
Shiplayer <lllytep199515@yandex.ru>
2016-04-23 23:24:02 +0300

    changing tabs on spaces

--- WavReading.jl
+++ WavReading.jl
@@ -3,70 +3,70 @@ using WAV
 using MFCC
 
 function splitArrayq(x)
-	new_channel = Array{Float64, 1}()
-	new_other_channel = Array{Float64, 1}()
-	points = Array{Int64, 1}()
-	flag = 1
-	count = 1
-	flag_1 = 0
-	for i = 2:size(x, 1)
-		if(abs(x[i, 1]) > 0.04)
-			if(flag == 1)
-				push!(points, size(new_channel)[1] + 1)
-				flag = 0
-				count = 0
-			end
-			push!(new_channel, x[i, 1])
-			push!(new_other_channel, x[i, 2])
-		elseif(count <= 500)
-			if(abs(x[i-1, 1]) < 0.04 && flag_1 == 1)
-				count = count + 1
-			elseif(flag_1 == 1 && abs(x[i - 1, 1]) > 0.04)
-				count = 0
-				flag_1 = 0
-			else
-				flag_1 = 1
-			end
-		elseif(count > 500)
-			count = count + 1
-			if(flag == 0)
-				push!(points, size(new_channel)[1])
-				flag = 1
-			end
-		end	
-	end
-	print("size: ", size(new_channel, 1), " ", size(new_other_channel, 1), "\n")
-	origin = Array{Float64, 2}(size(new_channel)[1], 2)
-	for i = 1:size(origin, 1)
-		origin[i, 1] = new_channel[i]
-		origin[i, 2] = new_other_channel[i]
-	end
-	return origin, points
+    new_channel = Array{Float64, 1}()
+    new_other_channel = Array{Float64, 1}()
+    points = Array{Int64, 1}()
+    flag = 1
+    count = 1
+    flag_1 = 0
+    for i = 2:size(x, 1)
+        if(abs(x[i, 1]) > 0.04)
+            if(flag == 1)
+                push!(points, size(new_channel)[1] + 1)
+                flag = 0
+                count = 0
+            end
+            push!(new_channel, x[i, 1])
+            push!(new_other_channel, x[i, 2])
+        elseif(count <= 500)
+            if(abs(x[i-1, 1]) < 0.04 && flag_1 == 1)
+                count = count + 1
+            elseif(flag_1 == 1 && abs(x[i - 1, 1]) > 0.04)
+                count = 0
+                flag_1 = 0
+            else
+                flag_1 = 1
+            end
+        elseif(count > 500)
+            count = count + 1
+            if(flag == 0)
+                push!(points, size(new_channel)[1])
+                flag = 1
+            end
+        end 
+    end
+    print("size: ", size(new_channel, 1), " ", size(new_other_channel, 1), "\n")
+    origin = Array{Float64, 2}(size(new_channel)[1], 2)
+    for i = 1:size(origin, 1)
+        origin[i, 1] = new_channel[i]
+        origin[i, 2] = new_other_channel[i]
+    end
+    return origin, points
 end
 
 function transformToHistogram(x_plot, y_plot)
-	x = Array{Float64, 2}(size(x_plot, 1) * 3, size(x_plot, 2) * 3)
-	y = Array{Float64, 2}(size(y_plot, 1) * 3, size(y_plot, 2) * 3)
-	print(x[1, 1])
-	for i = 1:size(x_plot, 1)
-		if(abs(x_plot[i, 1]) < 1000)
-			x[i * 3 - 1, 1] = abs(x_plot[i, 1])
-			x[i * 3 - 1, 2] = abs(x_plot[i, 2])
-			y[i * 3 - 1, 1], y[i * 3 - 1, 2] = y_plot[i, 1], y_plot[i, 2]
-			x[i * 3 - 2, 1], x[i * 3 - 2, 2] = abs(x_plot[i ,1]), abs(x_plot[i, 2])
-			y[i * 3 - 2, 1], y[i * 3 - 2, 2] = 0, 0
-			x[i * 3, 1], x[i * 3, 2] = abs(x_plot[i, 1]), abs(x_plot[i, 2])
-			y[i * 3, 1], y[i * 3, 2] = 0, 0 
-		else
-			x[i * 3, 1], x[i * 3, 2] = 0, 0
-			x[i * 3 - 1, 1], x[i * 3 - 1, 2] = 0, 0
-			x[i * 3 - 2, 1], x[i * 3 - 2, 2] = 0, 0
-			y[i * 3, 1], y[i * 3, 2] = 0, 0 
-			y[i * 3 - 1, 1], y[i * 3 - 1, 2] = 0, 0 
-			y[i * 3 - 2, 1], y[i * 3 - 2, 2] = 0, 0 
-		end
-	end
-	return x, y
+    x = Array{Float64, 2}(size(x_plot, 1) * 3, size(x_plot, 2) * 3)
+    y = Array{Float64, 2}(size(y_plot, 1) * 3, size(y_plot, 2) * 3)
+    print(x[1, 1])
+    for i = 1:size(x_plot, 1)
+        if(abs(x_plot[i, 1]) < 1000)
+            x[i * 3 - 1, 1] = abs(x_plot[i, 1])
+            x[i * 3 - 1, 2] = abs(x_plot[i, 2])
+            y[i * 3 - 1, 1], y[i * 3 - 1, 2] = y_plot[i, 1], y_plot[i, 2]
+            x[i * 3 - 2, 1], x[i * 3 - 2, 2] = abs(x_plot[i ,1]), abs(x_plot[i, 2])
+            y[i * 3 - 2, 1], y[i * 3 - 2, 2] = 0, 0
+            x[i * 3, 1], x[i * 3, 2] = abs(x_plot[i, 1]), abs(x_plot[i, 2])
+            y[i * 3, 1], y[i * 3, 2] = 0, 0 
+        else
+            x[i * 3, 1], x[i * 3, 2] = 0, 0
+            x[i * 3 - 1, 1], x[i * 3 - 1, 2] = 0, 0
+            x[i * 3 - 2, 1], x[i * 3 - 2, 2] = 0, 0
+            y[i * 3, 1], y[i * 3, 2] = 0, 0 
+            y[i * 3 - 1, 1], y[i * 3 - 1, 2] = 0, 0 
+            y[i * 3 - 2, 1], y[i * 3 - 2, 2] = 0, 0 
+        end
+    end
+    return x, y
 end
 
 WavPath = "data/orig/arctic_a0001.wav"
@@ -80,17 +80,17 @@ print("length(x): ",length(x), "\n")
 x_words = Array{Float64, 1}()
 x_words_other_channel = Array{Float64, 1}()
 if(size(points)[1] >= 2)
-	for i = points[1]:points[2]
-		push!(x_words, x[i, 1])
-		push!(x_words_other_channel, x[i, 2])
-#		x_words[i, 1], x_words[i, 2] = x[i, 1], x[i, 2]
-	end
+    for i = points[1]:points[2]
+        push!(x_words, x[i, 1])
+        push!(x_words_other_channel, x[i, 2])
+#       x_words[i, 1], x_words[i, 2] = x[i, 1], x[i, 2]
+    end
 end
 print("length: ",length(x_words), "\n")
 words = Array{Float64, 2}(length(x_words), 2)
 for i=1:length(x_words)
-	words[i, 1] = x_words[i]
-	words[i, 2] = x_words_other_channel[i]
+    words[i, 1] = x_words[i]
+    words[i, 2] = x_words_other_channel[i]
 end
 print("print: ", size(words, 1), " ", size(words, 2), "\n")
 plot(words, "r")
@@ -101,9 +101,9 @@ wavplay(words, fs/2)
 print(typeof(coordinates))
 print(size(coordinates,1), " ", size(coordinates,2), "\n")
 #for i = 1 : size(coordinates, 1)
-#	push!(x, coordinates[i ,1])
-#	push!(y, coordinates[i, 2])
-#	print("coordinates[", i ,", 1] = ", coordinates[i,1], " ", "coordinates[", i, ",2] = ", coordinates[i,2], "\n")
+#   push!(x, coordinates[i ,1])
+#   push!(y, coordinates[i, 2])
+#   print("coordinates[", i ,", 1] = ", coordinates[i,1], " ", "coordinates[", i, ",2] = ", coordinates[i,2], "\n")
 #end
 X = fft(coordinates)
 print(size(X, 1), " ", size(X, 2), "\n")
@@ -113,10 +113,10 @@ plot(abs(x), abs(y * 100), "r")
 show()
 
 function f(m)
-	
+    
 end
 
 function B(b)
-	return 700*(exp(b/1125) - 1)
+    return 700*(exp(b/1125) - 1)
 end
 =#
