8b7ad27264282305fc67ea2fbef0597441470784
Shiplayer <lllytep199515@yandex.ru>
2016-06-16 09:20:43 +0300

    learn

--- NeuralNetwork/MFCCNetworkLearn.jl
+++ NeuralNetwork/MFCCNetworkLearn.jl
@@ -6,12 +6,12 @@ MEMORYPATH = "Memory/Str2MFCC.txt"
 memory = open(MEMORYPATH, "r")
 
 for l in eachline(memory)
-    line = split(l, "=");
+    global line = split(l, "=");
     mfcc = line[2];
     if(OS_NAME == :Windows)
         mfcc = chop(mfcc)
     end
-    coeff = [parse(i) for i in split(mfcc[2:end-1], " ")]
+    coeff = [parse(i) for i in split(mfcc[2:end-2], ",")]
 end
 
 println(length(dict));
